83495e013349969b54ee1989b4bb7462885f3868
tomizawa0214 <j.tomizawa.japan@gmail.com>
2021-02-13 18:12:41 +0900

    文字起こし

--- mojiokoshi.py
+++ mojiokoshi.py
@@ -6,41 +6,29 @@ import threading
 
 r = sr.Recognizer() #言葉を認識するオブジェクト
 mic = sr.Microphone() #マイクオブジェクト
-result_list = [] #エクセルに出力するデータを格納するリスト
-endFlag = False #記録終了フラグ
-lock = threading.RLock() #ロックオブジェクトの生成
+# result_list = [] #エクセルに出力するデータを格納するリスト
+# endFlag = False #記録終了フラグ
+# lock = threading.RLock() #ロックオブジェクトの生成
 
-def speechToText(): #マイクから音声を取り込み続ける
-    print("記録を開始しました。終了するには「記録を終了」と言ってください。")
-    while True:
-        with mic as source:
-            r.adjust_for_ambient_noise(source) #ノイズ対応
-            audio  = r.listen(source) #オーディオファイルに変換
-        threading.Thread(target=speechRecognize,args=(lock,audio)).start()
-        if endFlag == True:
+while True:
+    print("記録を開始します。何か喋ってください。")
+
+    with mic as source:
+        r.adjust_for_ambient_noise(source) #雑音対策
+        audio = r.listen(source)
+
+    print ("認識中…")
+
+    try:
+        print(r.recognize_google(audio, language='ja-JP'))
+
+        # "ストップ" と言ったら音声認識を止める
+        if r.recognize_google(audio, language='ja-JP') == "ストップ" :
+            print("end")
             break
-    print("プログラム終了")
 
-def speechRecognize(lock,audio): #音声を認識し文字列に変換する
-    global endFlag
-    if endFlag == True: #記録を終了と言われた場合は認識を行わない
-        return
-    with lock:
-        try:
-            print ("認識中")
-            result = r.recognize_google(audio, language='ja-JP') #音声を文字に変換
-            now = dt.datetime.today()
-            if result == "記録を終了": 
-                fileName = str(now)+"会議メモ.xlsx" #保存するファイル名
-                df = pd.DataFrame(result_list,columns=['時刻', '内容']) #列名
-                with pd.ExcelWriter(fileName) as writer:
-                    df.to_excel(writer,index=False) #エクセルファイルに書き出し
-                print(fileName+"という名前で保存しました。")
-                endFlag = True
-            else:
-                print(str(now)+result) #認識結果を表示
-                result_list.append([now,result]) #入力したデータをリスト形式で追加
-        except:
-            pass
-        
-speechToText()
+    # 以下は認識できなかったときに止まらないように。
+    except sr.UnknownValueError:
+        print("could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
