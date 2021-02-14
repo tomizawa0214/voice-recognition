318aff367134411488b4bc60b15d7b30e2652181
tomizawa0214 <j.tomizawa.japan@gmail.com>
2021-02-14 22:17:04 +0900

    テキスト保存

--- mojiokoshi.py
+++ mojiokoshi.py
@@ -1,23 +1,26 @@
-import pandas as pd #エクセルのブックを操作するライブラリの読み込み
 import speech_recognition as sr
-import datetime as dt
-import threading
+from datetime import datetime
+import os
 
+
+#文字起こしファイルのファイル名を日付のtxtファイルとする
+filename = datetime.now().strftime('%Y%m%d_%H%M')
+txt = filename + ".txt"
+
+with open(txt, 'w') as f: #txtファイルの新規作成 
+    f.write(filename + "\n") #最初の一行目にはfilenameを記載する
 
 r = sr.Recognizer() #言葉を認識するオブジェクト
 mic = sr.Microphone() #マイクオブジェクト
-# result_list = [] #エクセルに出力するデータを格納するリスト
-# endFlag = False #記録終了フラグ
-# lock = threading.RLock() #ロックオブジェクトの生成
 
 while True:
-    print("記録を開始します。何か喋ってください。")
+    print("記録を開始するよ！何か喋ってね！")
 
     with mic as source:
         r.adjust_for_ambient_noise(source) #雑音対策
         audio = r.listen(source)
 
-    print ("認識中…")
+    print ("考え中…")
 
     try:
         print(r.recognize_google(audio, language='ja-JP'))
@@ -26,9 +29,11 @@ while True:
         if r.recognize_google(audio, language='ja-JP') == "ストップ" :
             print("end")
             break
+        with open(txt,'a') as f: #ファイルの末尾に追記していく
+            f.write("\n" + r.recognize_google(audio, language='ja-JP'))
 
     # 以下は認識できなかったときに止まらないように。
     except sr.UnknownValueError:
-        print("could not understand audio")
+        print("すみません、ちょっと何言ってるかわかりません。")
     except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        print("すみません、ちょっと何言ってるかわかりません。; {0}".format(e))
